4aa2fcc29b67449dcd26dfc1b57601e9ec857cb3
noveria <jaybono30@hotmail.com>
2017-12-29 12:53:10 +0000

    Added Integration Test

--- integration-test/src/test/java/com/noveria/integration/MusicRepositoryIT.java
+++ integration-test/src/test/java/com/noveria/integration/MusicRepositoryIT.java
@@ -4,6 +4,7 @@ import com.noveria.integration.application.TestApplication;
 import com.noveria.integration.asserter.WaitForHelper;
 import com.noveria.integration.asserter.WaitForService;
 import com.noveria.integration.response.musicrepo.AlbumResponse;
+import com.noveria.integration.response.musicrepo.ArtistResponse;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -55,10 +56,35 @@ public class MusicRepositoryIT {
                         new ParameterizedTypeReference<List<AlbumResponse>>() {});
 
         List<AlbumResponse> albums = getAllAlbumsResponse.getBody();
-        assertEquals(1, albums.size());
+
+        assertEquals(2, albums.size());
+
         assertEquals(1L, albums.get(0).getId().longValue());
         assertEquals("The Joshua Tree", albums.get(0).getName());
         assertEquals("Ground Breaking Album", albums.get(0).getDescription());
+
+        assertEquals(2L, albums.get(1).getId().longValue());
+        assertEquals("Achtung Baby", albums.get(1).getName());
+        assertEquals("90s Album", albums.get(1).getDescription());
+    }
+
+    @Test
+    public void getArtistUrl_returns_expected_albums() {
+        String artistUrl = musicRepoUrl+"/artist";
+
+        logger.info("calling url: {}",artistUrl);
+
+        ResponseEntity<List<ArtistResponse>> getAllArtistsResponse =
+                restTemplate.exchange(artistUrl,HttpMethod.GET, null,
+                        new ParameterizedTypeReference<List<ArtistResponse>>() {});
+
+        List<ArtistResponse> artists = getAllArtistsResponse.getBody();
+
+        assertEquals(1, artists.size());
+
+        assertEquals(1L, artists.get(0).getId().longValue());
+        assertEquals("U2", artists.get(0).getName());
+        assertEquals("Irish Rock Band", artists.get(0).getDescription());
     }
 
 }
--- music-repository/src/main/resources/db/migration/V2__Add_Seed_Data.sql
+++ music-repository/src/main/resources/db/migration/V2__Add_Seed_Data.sql
@@ -6,3 +6,21 @@ VALUES ('The Joshua Tree', 'Ground Breaking Album', SELECT id FROM artist WHERE 
 
 INSERT INTO track (NAME, ALBUM_ID)
 VALUES ('Where The Streets Have No Name',  SELECT id FROM album WHERE name = 'The Joshua Tree');
+
+INSERT INTO track (NAME, ALBUM_ID)
+VALUES ('With or Without You',  SELECT id FROM album WHERE name = 'The Joshua Tree');
+
+INSERT INTO track (NAME, ALBUM_ID)
+VALUES ('One Tree Hill',  SELECT id FROM album WHERE name = 'The Joshua Tree');
+
+INSERT INTO album (NAME, DESCRIPTION, ARTIST_ID)
+VALUES ('Achtung Baby', '90s Album', SELECT id FROM artist WHERE name = 'U2');
+
+INSERT INTO track (NAME, ALBUM_ID)
+VALUES ('Even Better Than The Real Thing',  SELECT id FROM album WHERE name = 'Achtung Baby');
+
+INSERT INTO track (NAME, ALBUM_ID)
+VALUES ('The Fly',  SELECT id FROM album WHERE name = 'Achtung Baby');
+
+INSERT INTO track (NAME, ALBUM_ID)
+VALUES ('One',  SELECT id FROM album WHERE name = 'Achtung Baby');
