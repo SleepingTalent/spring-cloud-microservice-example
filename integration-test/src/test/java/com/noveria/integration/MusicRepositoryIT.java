bbf0c2a022ce789f72ee20167ab9423482c79ef8
noveria <jaybono30@hotmail.com>
2017-12-29 12:31:05 +0000

    Added Integration Test

--- integration-test/src/test/java/com/noveria/integration/MusicRepositoryIT.java
+++ integration-test/src/test/java/com/noveria/integration/MusicRepositoryIT.java
@@ -3,6 +3,7 @@ package com.noveria.integration;
 import com.noveria.integration.application.TestApplication;
 import com.noveria.integration.asserter.WaitForHelper;
 import com.noveria.integration.asserter.WaitForService;
+import com.noveria.integration.response.musicrepo.AlbumResponse;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,9 +12,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -44,9 +50,15 @@ public class MusicRepositoryIT {
 
         logger.info("calling url: {}",albumUrl);
 
-       String response = restTemplate.getForObject(albumUrl,String.class);
-       assertEquals("[{\"id\":1,\"name\":\"The Joshua Tree\",\"description\":\"Ground Breaking Album\"}]",
-               response);
+        ResponseEntity<List<AlbumResponse>> getAllAlbumsResponse =
+                restTemplate.exchange(albumUrl,HttpMethod.GET, null,
+                        new ParameterizedTypeReference<List<AlbumResponse>>() {});
+
+        List<AlbumResponse> albums = getAllAlbumsResponse.getBody();
+        assertEquals(1, albums.size());
+        assertEquals(1L, albums.get(0).getId().longValue());
+        assertEquals("The Joshua Tree", albums.get(0).getName());
+        assertEquals("Ground Breaking Album", albums.get(0).getDescription());
     }
 
 }
