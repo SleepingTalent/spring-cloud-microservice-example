dcac302f4ca1659c202d61487c736a728795261f
noveria <jaybono30@hotmail.com>
2018-04-03 15:45:43 +0100

    updated spring cloud to camden

--- music-repository/src/main/java/com/noveria/musicrepository/config/ZipkinConfiguration.java
+++ music-repository/src/main/java/com/noveria/musicrepository/config/ZipkinConfiguration.java
@@ -5,53 +5,58 @@ import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.sleuth.metric.SpanMetricReporter;
-import org.springframework.cloud.sleuth.zipkin.HttpZipkinSpanReporter;
-import org.springframework.cloud.sleuth.zipkin.ZipkinProperties;
-import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+//import org.springframework.cloud.sleuth.zipkin.HttpZipkinSpanReporter;
+//import org.springframework.cloud.sleuth.zipkin.ZipkinProperties;
+//import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import zipkin.Span;
+//import zipkin.Span;
 
 @Configuration
 public class ZipkinConfiguration {
 
-    @Autowired
-    private EurekaClient eurekaClient;
-
-    @Autowired
-    private SpanMetricReporter spanMetricReporter;
-
-    @Autowired
-    private ZipkinProperties zipkinProperties;
-
-    @Value("${spring.sleuth.web.skipPattern}")
-    private String skipPattern;
-
-    @Bean
-    public ZipkinSpanReporter makeZipkinSpanReporter() {
-        return new ZipkinSpanReporter() {
-            private HttpZipkinSpanReporter delegate;
-            private String baseUrl;
-
-            @Override
-            public void report(Span span) {
-
-                InstanceInfo instance = eurekaClient.getNextServerFromEureka("zipkin-service", false);
-
-                if (!(baseUrl != null && instance.getHomePageUrl().equals(baseUrl))) {
-
-                    baseUrl = instance.getHomePageUrl();
-
-                    delegate = new HttpZipkinSpanReporter(baseUrl,
-                            zipkinProperties.getFlushInterval(),
-                            zipkinProperties.getCompression().isEnabled(),
-                            spanMetricReporter);
-
-                    if (!span.name.matches(skipPattern)){
-                        delegate.report(span);
-                    }
-                }
-            }
-        };
-    }
+//    @Bean
+//    public AlwaysSampler defaultSampler() {
+//        return new AlwaysSampler();
+//    }
+//    @Autowired
+//    private EurekaClient eurekaClient;
+//
+//    @Autowired
+//    private SpanMetricReporter spanMetricReporter;
+//
+//    @Autowired
+//    private ZipkinProperties zipkinProperties;
+//
+//    @Value("${spring.sleuth.web.skipPattern}")
+//    private String skipPattern;
+//
+//    @Bean
+//    public ZipkinSpanReporter makeZipkinSpanReporter() {
+//        return new ZipkinSpanReporter() {
+//            private HttpZipkinSpanReporter delegate;
+//            private String baseUrl;
+//
+//            @Override
+//            public void report(Span span) {
+//
+//                InstanceInfo instance = eurekaClient.getNextServerFromEureka("zipkin-service", false);
+//
+//                if (!(baseUrl != null && instance.getHomePageUrl().equals(baseUrl))) {
+//
+//                    baseUrl = instance.getHomePageUrl();
+//
+//                    delegate = new HttpZipkinSpanReporter(baseUrl,
+//                            zipkinProperties.getFlushInterval(),
+//                            zipkinProperties.getCompression().isEnabled(),
+//                            spanMetricReporter);
+//
+//                    if (!span.name.matches(skipPattern)){
+//                        delegate.report(span);
+//                    }
+//                }
+//            }
+//        };
+//    }
 }
